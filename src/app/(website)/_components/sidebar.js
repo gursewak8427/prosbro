1226d54913dfaea031432bcaf159d70fcfaf898e
Abhinandan411 <abhinandankumar6204@gmail.com>
2024-07-11 15:59:52 +0530

    added side bar and my project page

--- src/app/(website)/_components/sidebar.js
+++ src/app/(website)/_components/sidebar.js
@@ -1,35 +1,53 @@
 // Sidebar.js
 import React from 'react';
-import { BeakerIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link';
+import GridViewIcon from '@mui/icons-material/GridView';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import RequestQuoteIcon from '@mui/icons-material/RequestQuote';
+import BentoIcon from '@mui/icons-material/Bento';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import SettingsIcon from '@mui/icons-material/Settings';
+import HelpIcon from '@mui/icons-material/Help';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 
 const Sidebar = () => {
   const items = [
-    { text: 'My projects', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Overview', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Invoices', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Templates & costs', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'My team', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Contacts', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Timesheet', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Daily logs', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Notifications', icon: <BeakerIcon className="h-5 w-5" /> },
-    { text: 'Settings', icon: <BeakerIcon className="h-5 w-5" /> },
+    { text: 'My projects', icon: <GridViewIcon className="h-5 w-5" />, href: '/my-projects' },
+    { text: 'Overview', icon: <SignalCellularAltIcon className="h-5 w-5" />, href: '/overview' },
+    { text: 'Invoices', icon: <RequestQuoteIcon className="h-5 w-5" />, href: '/invoices' },
+    { text: 'Templates & costs', icon: <BentoIcon className="h-5 w-5" />, href: '/templates-costs' },
+    { text: 'My team', icon: <PeopleAltIcon className="h-5 w-5" />, href: '/team' },
+    { text: 'Contacts', icon: <ContactsIcon className="h-5 w-5" />, href: '/contacts' },
+    { text: 'Timesheet', icon: <PendingActionsIcon className="h-5 w-5" />, href: '/timesheet' },
+    { text: 'Daily logs', icon: <EventNoteIcon className="h-5 w-5" />, href: '/daily-logs' },
+    { text: 'Notifications', icon: <NotificationsActiveIcon className="h-5 w-5" />, href: '/notifications' },
+    { text: 'Settings', icon: <SettingsIcon className="h-5 w-5" />, href: '/settings' },
   ];
 
   return (
-    <div className="w-60 bg-gray-100 h-screen">
+    <div className="w-60  h-full bg-slate-50">
       <div className="text-xl font-semibold p-4">Billdr PRO</div>
-      <ul>
+      <ul className='px-2 '>
         {items.map((item, index) => (
-          <li key={index} className="flex items-center p-4 hover:bg-gray-200 cursor-pointer">
-            <span className="mr-4">{item.icon}</span>
-            <span>{item.text}</span>
-          </li>
+          <Link href={item.href} key={index}>
+            <li className="group flex items-center font-semibold p-4 hover:bg-gray-200 cursor-pointer rounded-lg ">
+              <span className="mr-4 group-hover:text-indigo-500 ">{item.icon}</span>
+              <span>{item.text}</span>
+            </li>
+          </Link>
         ))}
       </ul>
-      <div className="border-t p-4">
-        <div className="text-sm">Gurvinder Singh</div>
+      <div className="border-t p-2 mt-10  bg-white ">
+        <div className="text-sm">
+          <h2 className='text-md items-center hover:bg-gray-200 cursor-pointer p-4 rounded-lg'><HelpIcon /> Help</h2>
+          <h2 className='text-md items-center hover:bg-gray-200 cursor-pointer p-4 rounded-lg'>< AccountCircleIcon className='text-indigo-500' /> Gurvinder Singh</h2>
+        </div>
       </div>
     </div>
   );
--- src/app/(website)/my-projects/page.js
+++ src/app/(website)/my-projects/page.js
@@ -1,5 +1,10 @@
 // MainContent.js
 import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import GridViewIcon from '@mui/icons-material/GridView';
+import MapIcon from '@mui/icons-material/Map';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const createData = (ref, address, tag, date, size, status) => {
   return { ref, address, tag, date, size, status };
@@ -11,11 +16,63 @@ const rows = [
 
 const MainContent = () => {
   return (
-    <div className="p-6 flex-1">
-      <div className="text-2xl font-bold mb-4">My projects</div>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4">Add a new project</button>
+    <div className="p-6 flex-1 bg-gray-200 shadow-md">
+
+      <div className='flex justify-between'>
+        <div>
+          <div className="flex gap-4 text-2xl font-bold mb-4">
+            <h1>My Projects </h1>
+            <p className='flex items-center justify-center text-xs p-2 bg-gray-300 text-center rounded-lg h-full'>Project-1</p>
+          </div>
+        </div>
+        <div>
+          <button className=" text-white px-4 py-2 rounded mb-4 bg-indigo-600 hover:bg-indigo-700 duration-200 "> + Add a new project</button>
+        </div>
+
+      </div>
+
+      <div className='flex justify-between mt-4 mb-4'>
+        <div className="p-2 border-b  border-gray-400">
+          <ul className="flex gap-4 text-gray-800 text-sm font-medium">
+            <li className="hover:text-indigo-500 cursor-pointer transition-colors duration-200">All projects</li>
+            <li className="hover:text-indigo-500 cursor-pointer transition-colors duration-200">To bid</li>
+            <li className="hover:text-indigo-500 cursor-pointer transition-colors duration-200">Quote sent</li>
+            <li className="hover:text-indigo-500 cursor-pointer transition-colors duration-200">In Construction</li>
+            <li className="hover:text-indigo-500 cursor-pointer transition-colors duration-200">Completed</li>
+            <li className="hover:text-indigo-500 cursor-pointer transition-colors duration-200">Archived</li>
+          </ul>
+        </div>
+
+
+        <div className='bg-white p-1 rounded-md'>
+          <ul className='flex gap-4'>
+            <li className='items-center p-2 space-x-2 hover:cursor-pointer hover:bg-slate-200 rounded-md transition-colors duration-200'><MenuIcon className='h-5 w-5' /> List</li>
+            <li className='items-center p-2 space-x-2 hover:cursor-pointer hover:bg-slate-200 rounded-md transition-colors duration-200'><GridViewIcon className='h-5 w-5' /> Grid</li>
+            <li className='items-center p-2 space-x-2 hover:cursor-pointer hover:bg-slate-200 rounded-md transition-colors duration-200'><MapIcon className='h-5 w-5' /> Map</li>
+          </ul>
+        </div>
+
+      </div>
+
+
+      <div className="flex gap-4 items-center w-1/2 mt-4 mb-4">
+        <input
+          type="text"
+          placeholder="Search a project, a client, an address"
+          className="p-2 border border-gray-300 rounded-md w-full"
+        />
+        <select className="p-2 border border-gray-300 rounded-md">
+          <option value="">Filter by tag</option>
+          <option value="project">Project</option>
+          <option value="client">Client</option>
+          <option value="address">Address</option>
+        </select>
+
+      </div>
+
+
       <div className="overflow-x-auto">
-        <table className="min-w-full bg-white">
+        <table className="min-w-full bg-white rounded-2xl">
           <thead>
             <tr>
               <th className="py-2 px-4 border">Ref #</th>
@@ -39,6 +96,18 @@ const MainContent = () => {
             ))}
           </tbody>
         </table>
+        <div className="flex  items-center p-2 bg-white rounded-2xl  justify-between mt-5 mb-5">
+          <p className="text-gray-400 text-sm font-medium">Showing Results</p>
+          <div className="flex items-center gap-2">
+            <button className="flex items-center justify-center p-2 bg-gray-200 rounded-md hover:bg-gray-300 transition-colors duration-200">
+              <KeyboardArrowLeftIcon className="h-5 w-5 text-gray-600" />
+            </button>
+            <p className="text-gray-800 text-sm font-medium">1</p>
+            <button className="flex items-center justify-center p-2 bg-gray-200 rounded-md hover:bg-gray-300 transition-colors duration-200">
+              <KeyboardArrowRightIcon className="h-5 w-5 text-gray-600" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
