71548f055828f2855dcf50e7882d65cc4813cbbc
Abhinandan411 <abhinandankumar6204@gmail.com>
2024-07-13 14:22:18 +0530

    added templates & costs

---
+++ src/app/(website)/templates-costs/page.js
@@ -0,0 +1,204 @@
+import React from 'react'
+
+export default function
+    () {
+    return (
+        <div className='p-6 flex-1 bg-gray-200 shadow-md'>
+            <div>
+                <h1 className="text-3xl font-bold">Templates</h1>
+                <div className='mt-5 mb-5'>
+                    <ul className='w-1/2 flex gap-4 border border-b-gray-400'>
+                        <li className='text-gray-700 border-2 border-b-indigo-600'>Billdr templates</li>
+                        <li className='text-gray-700'>My templates</li>
+                        <li className='text-gray-700'>My cost catalog</li>
+                        <li className='text-gray-700'>Categories</li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <div className='flex'>
+                <button className="text-white px-4 py-2 rounded-lg bg-indigo-600 hover:bg-indigo-700 duration-200 ml-auto">
+                    + Create a new template
+                </button>
+            </div>
+
+            <div className='mt-5 mb-5'>
+                <div className='flex flex-wrap gap-4'>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.PUuevUFoxDO5lRMcQBdQnAHaE8?w=280&h=187&c=7&r=0&o=5&pid=1.7"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">New Build</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://de927adv5b23k.cloudfront.net/wp-content/uploads/2018/06/29154124/kitchen-nakul-baghel.jpeg"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">Kitchen re-modal standard</h1>
+                                <p className="text-gray-600 text-sm mt-2">Renovation of standard low to mid-end kitchen</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$36k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP._LLumzR0wnTykqBEHaq7pAHaFC?w=182&h=124&c=7&r=0&o=5&pid=1.7"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">Bathroom Remodel- Heigh End 10X10</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$26k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.BNJ4V6jItBKycrHY2ZJb1gAAAA?rs=1&pid=ImgDetMain"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">New Build</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.mbiP8QOpyoMe8bdnmm-zKwHaE8?rs=1&pid=ImgDetMain"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">Bathroom Remodel- Heigh End 5X5</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.PUuevUFoxDO5lRMcQBdQnAHaE8?w=280&h=187&c=7&r=0&o=5&pid=1.7"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">New Build</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.PUuevUFoxDO5lRMcQBdQnAHaE8?w=280&h=187&c=7&r=0&o=5&pid=1.7"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">New Build</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.PUuevUFoxDO5lRMcQBdQnAHaE8?w=280&h=187&c=7&r=0&o=5&pid=1.7"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">New Build</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.PUuevUFoxDO5lRMcQBdQnAHaE8?w=280&h=187&c=7&r=0&o=5&pid=1.7"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">New Build</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+                    <div className="flex bg-white basis-full md:basis-[calc(50%-1rem)] p-4 rounded-lg shadow-lg">
+                        <img
+                            src="https://th.bing.com/th/id/OIP.PUuevUFoxDO5lRMcQBdQnAHaE8?w=280&h=187&c=7&r=0&o=5&pid=1.7"
+                            alt="House"
+                            className="w-1/3 h-auto rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col justify-between ml-4 w-full">
+                            <div>
+                                <h1 className="text-xl font-semibold">New Build</h1>
+                                <p className="text-gray-600 text-sm mt-2">Contemporary wood frame house on a concrete foundation</p>
+                            </div>
+
+                        </div>
+                        <div className="flex justify-end">
+                            <h1 className="text-2xl font-bold text-indigo-600">$193k</h1>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
